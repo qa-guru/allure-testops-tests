3d9b52c8ceb183a7a6130879cba7da47d16562e8
Andrei Kaliasinski <andykolesinski@gmail.com>
2021-09-06 10:27:31 +0300

    Removed unused imports

--- src/test/java/cloud/autotests/tests/ProjectsListTests.java
+++ src/test/java/cloud/autotests/tests/ProjectsListTests.java
@@ -1,16 +1,9 @@
 package cloud.autotests.tests;
 
-import cloud.autotests.config.App;
 import cloud.autotests.helpers.WithLogin;
 import cloud.autotests.pages.ProjectsTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static io.qameta.allure.Allure.step;
 
 public class ProjectsListTests extends TestBase {
 
