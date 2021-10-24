4fba3880e660621eda5a5c8a201a1e7a6fc7a0e9
Александр <A.grachev94@yandex.ru>
2021-10-24 13:07:01 +0300

    поправил тест из-за изменений в testops

--- src/test/java/cloud/autotests/tests/ThemeTests.java
+++ src/test/java/cloud/autotests/tests/ThemeTests.java
@@ -1,10 +1,10 @@
 package cloud.autotests.tests;
 
-import cloud.autotests.helpers.WithLogin;
-import org.junit.jupiter.api.Test;
+import cloud.autotests.config.App;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.localStorage;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,37 +14,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ThemeTests extends TestBase {
 
-    private void preparingLocalStorage(String theme) {
-        localStorage().removeItem("AS_THEME");
-        localStorage().setItem("AS_THEME", theme);
-        refresh();
-    }
-
-    private String backgroundColor() {
-        return $("html")
-                .getAttribute("style")
-                .split("; ")[0]
-                .split(":")[1];
-    }
-
 
     @CsvSource(value = {
-            "light,dark,#1e1e1f",
+            "light,dark,#1e293b",
             "dark,light,#fff"
     })
-    @WithLogin
     @ParameterizedTest(name = "Change theme from {0} to {1}")
     void changeOfThemeTest(String beforeTheme, String afterTheme, String hexBackgroundColor) {
         step("Open the main page", () ->
                 open(""));
-        step("Install a " + beforeTheme + " theme", () ->
-                preparingLocalStorage(beforeTheme));
+        step("Install a " + beforeTheme + " theme", () -> {
+            localStorage().setItem("AS_THEME", beforeTheme);
+            refresh();
+        });
+        step("Fill login form", () -> {
+            $(byName("username")).setValue(App.config.userLogin());
+            $(byName("password")).setValue((App.config.userPassword()))
+                    .pressEnter();
+        });
         step("Click on the user menu", () ->
-                $(".SideMenu").$(".Avatar").click());
+                $("span.LogoWithPlaceholder").click());
         step("Choose a " + afterTheme + " theme", () ->
-                // Более простой локатор подобрать не удалось
-                $(".UserMenu__menu>.Menu__item:not(.Menu__item_separator):not(.Menu__item_info)").click());
+                $("label.UserMenu__checkbox").parent().click());
         step("checking the display of the dark theme", () ->
-                assertThat(backgroundColor()).isEqualTo(hexBackgroundColor));
+                assertThat(
+                        $("html")
+                        .getAttribute("style")
+                        .split("; ")[0]
+                        .split(":")[1]
+                )
+                        .isEqualTo(hexBackgroundColor));
     }
 }
