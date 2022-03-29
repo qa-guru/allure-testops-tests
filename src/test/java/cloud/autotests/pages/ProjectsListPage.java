e8ea3598b7408060a0c13360674b99c74084114f
sashkir7 <sashkir7@mail.ru>
2022-03-29 11:22:16 +0600

    Add test for find exist project

--- src/test/java/cloud/autotests/pages/ProjectsListPage.java
+++ src/test/java/cloud/autotests/pages/ProjectsListPage.java
@@ -3,10 +3,9 @@ package cloud.autotests.pages;
 import cloud.autotests.pages.components.CreateProjectPopup;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class ProjectsListPage {
@@ -26,11 +25,22 @@ public class ProjectsListPage {
         createProjectPopup.clickSubmitButton();
     }
 
+    @Step("Find project [{projectName}]")
+    public void findProject(String projectName) {
+        $("[type='search']").setValue(projectName).pressEnter();
+    }
+
+    // ToDo Refactoring
     public void filterProject(String projectName) {
         step("confirm the project {projectName} deletion", () -> {
             $("input.HomeLayout__search").setValue(projectName);
             sleep(500); // иначе через раз падает
         });
+    }
+
+    @Step("Verify project [{projectName}] contains in projects list")
+    public void checkThatProjectContainsInProjectsList(String projectName) {
+        $$(".ProjectRow__name").shouldHave(itemWithText(projectName));
     }
 
 }
