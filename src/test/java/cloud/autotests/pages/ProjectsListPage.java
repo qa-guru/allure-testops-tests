3ecb4bee31a17dd62caa4183c0e989abaa1846c8
sashkir7 <sashkir7@mail.ru>
2022-03-29 10:02:40 +0600

    Edit page and component

--- src/test/java/cloud/autotests/pages/ProjectsListPage.java
+++ src/test/java/cloud/autotests/pages/ProjectsListPage.java
@@ -3,7 +3,6 @@ package cloud.autotests.pages;
 import cloud.autotests.pages.components.CreateProjectPopup;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
@@ -12,7 +11,8 @@ import static io.qameta.allure.Allure.step;
 
 public class ProjectsListPage {
 
-    private CreateProjectPopup createProjectPopup = new CreateProjectPopup();
+    public final CreateProjectPopup createProjectPopup = new CreateProjectPopup();
+    public final ProjectsTable projectTable = new ProjectsTable();
 
     @Step("Open projects list page")
     public ProjectsListPage openPage() {
@@ -20,15 +20,11 @@ public class ProjectsListPage {
         return this;
     }
 
-    public ProjectsTable getProjectsTable() {
-        return new ProjectsTable();
-    }
-
     @Step("Create new project with name [{projectName}]")
     public void createNewProject(String projectName) {
         $(byText("New project")).click();
-        $(byName("name")).setValue(projectName);
-        $(byName("submit")).click();
+        createProjectPopup.fillNameField(projectName);
+        createProjectPopup.clickSubmitButton();
     }
 
     public void filterProject(String projectName) {
@@ -36,19 +32,6 @@ public class ProjectsListPage {
             $("input.HomeLayout__search").setValue(projectName);
             sleep(500); // иначе через раз падает
         });
-    }
-
-    @Step("Creating a new project from a main page")
-    public void createNewProjectWithError(String projectName) {
-        step("Click on a button 'New project'", () ->
-                $(byText("New project")).click()
-        );
-        step("Fill obligatory fields name with {projectName} and abbreviation with {projectAbbr}", () ->
-                $("input[name=name]").setValue(projectName)
-        );
-        step("Click submit, creating {projectName} project", () ->
-                $(byText("Submit")).click()
-        );
     }
 
 }
--- src/test/java/cloud/autotests/pages/components/CreateProjectPopup.java
+++ src/test/java/cloud/autotests/pages/components/CreateProjectPopup.java
@@ -4,11 +4,27 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CreateProjectPopup {
-    @Step("Check that error message is `{message}`")
-    public void checkErrorMessage(String message) {
-        $(".Form__error").shouldHave(text(message));
+
+    private final SelenideElement nameInput = $(byName("name"));
+
+    @Step("Fill field [Name] with value {projectName}")
+    public void fillNameField(String projectName) {
+        nameInput.setValue(projectName);
     }
+
+    @Step("Click [Submit] button")
+    public void clickSubmitButton() {
+        $(byName("submit")).click();
+    }
+
+    @Step("Verify empty name field has error message")
+    public void checkThatEmptyNameFieldHasErrorMessage() {
+        nameInput.closest("label").find(".Form__error")
+                .shouldHave(text("Name is required"));
+    }
+
 }
