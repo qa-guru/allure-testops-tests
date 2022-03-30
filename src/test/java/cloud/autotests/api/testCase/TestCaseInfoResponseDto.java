cc18a56e50406deb416e39f5d860480271a7181e
sashkir7 <sashkir7@mail.ru>
2022-03-31 00:00:05 +0600

    Remove @Builder annotation

--- src/test/java/cloud/autotests/api/testCase/TestCaseInfoResponseDto.java
+++ src/test/java/cloud/autotests/api/testCase/TestCaseInfoResponseDto.java
@@ -2,20 +2,19 @@ package cloud.autotests.api.testCase;
 
 import cloud.autotests.api.interfaces.ResponseDto;
 import com.google.gson.annotations.SerializedName;
-import lombok.Builder;
 import lombok.Getter;
 
-@Builder @Getter
+@Getter
 public class TestCaseInfoResponseDto implements ResponseDto {
 
     @SerializedName("name")
-    private final String testCaseName;
+    private String testCaseName;
 
     @SerializedName("projectId")
-    private final Integer projectId;
+    private Integer projectId;
 
     @SerializedName("id")
-    private final Integer testCaseId;
+    private Integer testCaseId;
 
     public static TestCaseInfoResponseDto fromJson(String json) {
         return gson.fromJson(json, TestCaseInfoResponseDto.class);
