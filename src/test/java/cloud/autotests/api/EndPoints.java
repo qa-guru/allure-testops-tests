359c86cbd6069e9e367851aa05042e5f045debb2
sashkir7 <sashkir7@mail.ru>
2022-03-30 23:59:50 +0600

    Add API methods and DTOs for defect endpoint

--- src/test/java/cloud/autotests/api/EndPoints.java
+++ src/test/java/cloud/autotests/api/EndPoints.java
@@ -2,12 +2,22 @@ package cloud.autotests.api;
 
 public class EndPoints {
 
+    // ToDo переименовать endpoins под конкретный запрос, то есть, например, DEFECT_CREATE
+        // использоваться будет в одном лишь месте
+
     // Projects
     public static final String PROJECT = "/api/rs/project";
     public static final String PROJECT_BY_ID = PROJECT + "/{id}";
 
+    // Test cases
+    public static final String TEST_CASE = "/api/rs/testcase";
+
+    // Defects
+    public static final String DEFECT_CREATE = "/api/rs/defect";
+    public static final String DEFECT_DELETE = DEFECT_CREATE + "/{id}";
+
     // Tags
-    public static final String TEST_CASE_TAG = "/api/rs/testcase/{testCaseId}/tag";
+    public static final String TEST_CASE_TAG = TEST_CASE + "/{testCaseId}/tag";
     public static final String TEST_TAG = "/api/rs/tag";
     public static final String TEST_TAG_BY_ID = TEST_TAG + "/{id}";
 
