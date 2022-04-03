b28588c35550e7b8819c1205e307ad74508db358
sashkir7 <sashkir7@mail.ru>
2022-04-03 20:31:15 +0600

    Small refactoring

--- src/test/java/cloud/autotests/pages/testCase/TestProperties.java
+++ src/test/java/cloud/autotests/pages/testCase/TestProperties.java
@@ -1,14 +1,14 @@
-package cloud.autotests.pages;
+package cloud.autotests.pages.testCase;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ElementsCollection;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class TestProperties {
+
     private ElementsCollection rows = $$(".TestCaseOverview__secondary > div");
     private ElementsCollection items = $$(".PaneSection__name");
 
@@ -21,4 +21,5 @@ public class TestProperties {
     public void shouldHaveElement(String itemName) {
         items.shouldHave(CollectionCondition.itemWithText(itemName));
     }
+
 }
--- src/test/java/cloud/autotests/tests/TestPropertiesTest.java
+++ src/test/java/cloud/autotests/tests/TestPropertiesTest.java
@@ -4,23 +4,21 @@ import cloud.autotests.data.MenuItem;
 import cloud.autotests.data.PropertyName;
 import cloud.autotests.helpers.WithLogin;
 import cloud.autotests.pages.*;
+import cloud.autotests.pages.testCase.TestProperties;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-
 @Story("Properties of Tests")
 public class TestPropertiesTest extends TestBase{
-    private static final String PROJECT_NAME = "teacher qa_guru_diplom_project";
+
+    // Test project [teacher qa_guru_diplom_project]
+    private static final int PROJECT_ID = 43;
     private static final String STATUS_NAME = "New";
 
     @WithLogin
     @Test
     void checkTestProperties(){
-        ProjectsListPage projectsListPage = open("", ProjectsListPage.class);
-        projectsListPage.filterProject(PROJECT_NAME);
-        ProjectsTable projectsTable = new ProjectsTable();
-        ProjectPage projectPage = projectsTable.navigateTo(PROJECT_NAME);
+        projectPage.openPage(PROJECT_ID);
         projectPage.getSidebar().navigateTo(MenuItem.TEST_CASES);
 
         TestCasesListPage testCasesTable = new TestCasesListPage();
@@ -37,6 +35,6 @@ public class TestPropertiesTest extends TestBase{
         testProperties.shouldHaveElement(PropertyName.MUTES.getDisplayedName());
         testProperties.shouldHaveElement(PropertyName.RELATIONS.getDisplayedName());
         testProperties.shouldHaveElement(PropertyName.DESCRIPTIONS.getDisplayedName());
-
     }
+
 }
