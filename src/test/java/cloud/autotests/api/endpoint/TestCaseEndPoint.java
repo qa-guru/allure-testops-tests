2204bd6dca33b7d63d209290ee6b2b1702c83bc0
sashkir7 <sashkir7@mail.ru>
2022-04-10 11:15:32 +0600

    Edit api endpoinst for test case tests

---
+++ src/test/java/cloud/autotests/api/endpoint/TestCaseEndPoint.java
@@ -0,0 +1,8 @@
+package cloud.autotests.api.endpoint;
+
+public class TestCaseEndPoint {
+
+    public static final String CREATE = "/api/rs/testcase";
+    public static final String DELETE = CREATE + "/{id}";
+
+}
