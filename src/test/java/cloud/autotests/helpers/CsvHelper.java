c4d29d1c16a69d35d4bf7e016c09daa79da079ea
sashkir7 <sashkir7@mail.ru>
2022-04-28 01:44:39 +0600

    Fix csv helper

--- src/test/java/cloud/autotests/helpers/CsvHelper.java
+++ src/test/java/cloud/autotests/helpers/CsvHelper.java
@@ -1,46 +1,22 @@
 package cloud.autotests.helpers;
 
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 import io.qameta.allure.Step;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CsvHelper {
 
-    private final CSVReader reader;
-
-    public CsvHelper(CSVReader reader) {
-        this.reader = reader;
-    }
-
-    public static CsvHelper of(File file) {
-        try {
-            FileInputStream inputStream = new FileInputStream(file);
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+    @Step("Read all strings from json file")
+    public static List<String[]> readAll(File file) {
+        try (FileInputStream inputStream = new FileInputStream(file);
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream)) {
             CSVReader reader = new CSVReader(inputStreamReader);
-            return new CsvHelper(reader);
-        } catch (IOException exception) {
-            throw new RuntimeException("File " + file.getAbsoluteFile() + " don't exist");
-        }
-    }
-
-    @Step("Convert csv to list")
-    public List<String[]> convertToList() {
-        try {
             return reader.readAll();
-        } catch (IOException | CsvException exception) {
-            return new ArrayList<>();
-        }
-    }
-
-    public void close() {
-        try {
-            reader.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            throw new RuntimeException("Error during parse file " + file.getAbsoluteFile());
         }
     }
 
--- src/test/java/cloud/autotests/tests/testCase/ExportTestCaseTests.java
+++ src/test/java/cloud/autotests/tests/testCase/ExportTestCaseTests.java
@@ -4,6 +4,7 @@ import cloud.autotests.helpers.CsvHelper;
 import cloud.autotests.helpers.WithLogin;
 import cloud.autotests.tests.BaseTest;
 import com.opencsv.exceptions.CsvException;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,7 @@ import java.util.List;
 import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Story("Export tests")
 public class ExportTestCaseTests extends BaseTest {
 
     private final static int PROJECT_ID = 1159;
@@ -22,20 +24,13 @@ public class ExportTestCaseTests extends BaseTest {
     @WithLogin
     @DisplayName("Export test case to csv")
     void exportTestCaseToCsv() throws IOException, CsvException {
-        // Test data
         int[] existTestCases = new int[] { 8751, 8752, 8753 };
 
-        // Arrange
         testCasesListPage.openPage(PROJECT_ID);
-
-        // Act
         testCasesListPage.selectTestCases(existTestCases);
         File file = testCasesListPage.exportToCsv();
-        CsvHelper csvReader = CsvHelper.of(file);
-        List<String[]> csvToList = csvReader.convertToList();
-        csvReader.close();
+        List<String[]> csvToList = CsvHelper.readAll(file);
 
-        // Assert
         // В ожидаемом результате + 1, тк есть заголовок таблицы
         step("Csv file should have size [" + existTestCases.length + "]", () ->
                 assertEquals(existTestCases.length + 1, csvToList.size()));
